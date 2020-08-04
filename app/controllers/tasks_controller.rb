f36d045291e723d8d6ea31a7ceb0513a2e945832
Edwin Babu <edwinbbu@gmail.com>
2020-08-04 18:04:53 +0530

    task creation support added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  
+
   def new
-    @task = Task.new
+    render
+  end
+
+  def create
+    puts task_params
+    @task = Task.new(task_params)
+    if @task.save
+      render status: :ok, json: { notice: 'Task was successfully created' }
+    else
+      errors = @task.errors.full_messages
+      render status: :unprocessable_entity, json: { errors: errors  }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
--- app/javascript/components/tasks/New.js
+++ app/javascript/components/tasks/New.js
@@ -3,21 +3,57 @@ import React, { Component } from 'react';
 class New extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      description: ""
+    };
   };
 
+  handleChange = (event) =>{
+    this.setState({
+      description: event.target.value
+    });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log('state:',this.state);
+    const payload = { task: { description: this.state.description } };
+    fetch("/tasks", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-CSRF-TOKEN": document.querySelector('[name="csrf-token"]').content
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(() => (window.location.href = "/tasks"))
+      .catch(function (err) {
+        if (error.text) {
+          error.text().then(err => {
+            console.error(err);
+          });
+        }
+      });
+  }
+  
   displayAddTaskForm() {
     return (
       <div>
         <div className="row">
           <h3 className="pb-3">Add Task</h3>
         </div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group row pt-3">
             <label htmlFor="name" className="col-sm-2 col-form-label">
               <h5 className="text-secondary ">Description: </h5>
             </label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" />
+              <input type="text" 
+              className="form-control" 
+              onChange={this.handleChange}
+              value={this.state.description}
+              />
             </div>
           </div>
           <div className="form-group row pt float-right pr-3">
